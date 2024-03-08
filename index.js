65f91f41942279ce6d80b5c46e92e4e433b21c01
rajcodes168h <raj19all@gmail.com>
2024-03-09 00:48:32 +0530

    env terms bash files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env. AWS_SECRET_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
 });
 const bodyParser = require('body-parser');
