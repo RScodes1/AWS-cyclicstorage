d9fcba24652384137352cf5ce29783c59add3ff0
rajcodes168h <raj19all@gmail.com>
2024-03-09 00:44:52 +0530

    env terms bash files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env. AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
