c2be4e7cc80c48b4d6ac57c4afaefeec7e7f580b
rajcodes168h <raj19all@gmail.com>
2024-03-10 00:04:12 +0530

    modified

--- index.js
+++ index.js
@@ -1,59 +1,102 @@
-const express = require('express');
-const app = express();
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
-const bodyParser = require('body-parser');
+// const express = require('express');
 require('dotenv').config()
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
+const {getSignedUrl} = require('@aws-sdk/s3-request-presigner');
+
+const s3Client = new S3Client({ 
+  region: process.env.AWS_REGION,
+  endpoint : "https://s3.console.aws.amazon.com/s3/buckets/myfirstawsbucket-rja?region=ap-southeast-1&bucketType=general&tab=objects",
+    crendentials : {
+      accessKeyId :  process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+ });
+
+// const app = express();
+// const PORT = 3000;
 
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname + "/index.html")
-})
-// curl -i https://some-app.cyclic.app/myFile.txt
-// app.get('*', async (req, res) => {
-//   let filename = req.path.slice(1);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-//   try {
-//     let s3File = await s3.getObject({
-//       Bucket: process.env.BUCKET,
-//       Key: filename,
-//     }).promise();
-
-//     res.set('Content-type', s3File.ContentType);
-//     res.send(s3File.Body.toString()).end();
-//   } catch (error) {
-//     if (error.code === 'NoSuchKey') {
-//       console.log(`No such key ${filename}`);
-//       res.sendStatus(404).end();
-//     } else {
-//       console.log(error);
-//       res.sendStatus(500).end();
-//     }
-//   }
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
 // });
 
-// POST route for uploading files
-app.post('/upload', async (req, res) => {
-  let filename = req.body.filename;
-  let content = req.body.content;
-      
-  try {
-    let base64Data = Buffer.from(content, 'base64');
-    await s3.putObject({
-        Body: JSON.stringify({key:"value"}),
-        Bucket: "cyclic-gray-bewildered-yak-ap-northeast-1",
-        Key: "some_files/my_file.json",
-    }).promise()
-    res.send('File uploaded successfully').end();
-    console.log();
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500).end();
-  }
-});
+async function getObjectURL(key){
+    
+  const command = new GetObjectCommand({
+      Bucket : "myfirstawsbucket-rja",
+      Key : key,
+  });
+  const url = await getSignedUrl(s3Client,command);
+  return url;
+}
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+async function putObject(filename){
+  
+  const command = new PutObjectCommand({
+    Bucket : "myfirstawsbucket-rja",
+    Key : `/uploads/user-uploads/${filename}`,
+    ContentType : "image/jpeg",
+  });
+  const url = await getSignedUrl(s3Client, command);
+  return url;
+}
+  
+      async function init(){
+  // console.log("URL for graphql.jpeg", await getObjectURL("56xsus.jpg"));
+   console.log("url for uplpading",await putObject(`image-${Date.now()}.jpeg`));
+}
+
+init();
+// Upload route
+// app.post('/upload', async(req, res) => {
+//     const photo = req.body.photo; //
+//     if (!photo) {
+//       return res.status(400).send('No photo provided');
+//   }
+//     const params = {
+//         Bucket: 'myfirstawsbucket-rja',
+//         Key: '5v6gwj.jpg',
+//         Body: Buffer.from(photo, 'base64'),
+//         ContentType: 'image/jpeg'
+//     };
+//     try {
+//         await s3Client.send(new PutObjectCommand(params));
+//         console.log('File uploaded successfully');
+//         res.send('File uploaded successfully');
+//     } catch (err) {
+//         console.error('Failed to upload photo:', err);
+//         res.status(500).send('Failed to upload photo');
+//     }
+// });
+
+// // Delete route
+// app.delete('/delete/:photoKey', async(req, res) => {
+//     const photoKey = req.params.photoKey;
+
+//     const params = {
+//         Bucket: 'myfirstawsbucket-rja',
+//         Key: photoKey
+//     };
+
+//  try {
+//         await s3Client.send(new DeleteObjectCommand(params));
+//         console.log('File deleted successfully');
+//         res.send('File deleted successfully');
+//     } catch (err) {
+//         console.error('Failed to delete photo:', err);
+//         res.status(500).send('Failed to delete photo');
+//     }
+// });
+
+// Start the server
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
